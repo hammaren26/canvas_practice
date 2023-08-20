0630f3a250f285e74232f1fff79cf4bccc214e92
Vyacheslav Tulaev <hammaren23@gmail.com>
2023-08-20 14:11:39 +0600

    движение по спирали картинки

--- src/debugJS.js
+++ src/debugJS.js
@@ -1,6 +1,7 @@
 
 window.addEventListener('load', eventWindowLoaded, false);
-let bullseye;
+
+var bullseye;
 
 function eventWindowLoaded() {
    bullseye = new Image();
@@ -9,11 +10,13 @@ function eventWindowLoaded() {
 }
 
 function eventAssetsLoaded() {
+
    canvasApp();
 }
 
 function canvasApp() {
-   let pointImage = new Image();
+
+   var pointImage = new Image();
    pointImage.src = "point.png";
    function drawScreen() {
       context.fillStyle = '#EEEEEE';
@@ -21,15 +24,15 @@ function canvasApp() {
       //Box
       context.strokeStyle = '#000000';
       context.strokeRect(1, 1, theCanvas.width - 2, theCanvas.height - 2);
-      let t = player.t;
-      let cx = 3 * (p1.x - p0.x)
-      let bx = 3 * (p2.x - p1.x) - cx;
-      let ax = p3.x - p0.x - cx - bx;
-      let cy = 3 * (p1.y - p0.y);
-      let by = 3 * (p2.y - p1.y) - cy;
-      let ay = p3.y - p0.y - cy - by;
-      let xt = ax * (t * t * t) + bx * (t * t) + cx * t + p0.x;
-      let yt = ay * (t * t * t) + by * (t * t) + cy * t + p0.y;
+      var t = player.t;
+      var cx = 3 * (p1.x - p0.x)
+      var bx = 3 * (p2.x - p1.x) - cx;
+      var ax = p3.x - p0.x - cx - bx;
+      var cy = 3 * (p1.y - p0.y);
+      var by = 3 * (p2.y - p1.y) - cy;
+      var ay = p3.y - p0.y - cy - by;
+      var xt = ax * (t * t * t) + bx * (t * t) + cx * t + p0.x;
+      var yt = ay * (t * t * t) + by * (t * t) + cy * t + p0.y;
       player.t += player.speed;
       if (player.t > 1) {
          player.t = 1;
@@ -64,9 +67,8 @@ function canvasApp() {
       context.fill();
       context.fillStyle = "#FFFFFF";
       context.fillText("3", p3.x - 2, p3.y + 2);
-      //Draw points to illustrate path
       points.push({ x: xt, y: yt });
-      for (let i = 0; i < points.length; i++) {
+      for (var i = 0; i < points.length; i++) {
          context.drawImage(pointImage, points[i].x, points[i].y, 1, 1);
       }
       context.closePath();
@@ -74,14 +76,15 @@ function canvasApp() {
       player.y = yt - bullseye.height / 2;
       context.drawImage(bullseye, player.x, player.y);
    }
-   let p0 = { x: 60, y: 10 };
-   let p1 = { x: 150, y: 350 };
-   let p2 = { x: 300, y: 375 };
-   let p3 = { x: 400, y: 20 };
-   let player = { x: 0, y: 0, speed: .01, t: 0 };
-   let points = new Array();
+   var p0 = { x: 150, y: 440 };
+   var p1 = { x: 450, y: 10 };
+   var p2 = { x: 50, y: 10 };
+   var p3 = { x: 325, y: 450 };
+   var player = { x: 0, y: 0, speed: .01, t: 0 };
+   var points = new Array();
    theCanvas = document.getElementById("canvasOne");
    context = theCanvas.getContext("2d");
+   ;
    function gameLoop() {
       window.setTimeout(gameLoop, 20);
       drawScreen()
